73b2e28f59ce36099bf93dc11324775023d75675
klierik <leo-tv@yandex.ru>
2016-07-20 16:11:25 +0300

    + update environment + added img.optimization for sprite.png + added task sequence

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -4,8 +4,9 @@ var gulp    = require('gulp-help')(require('gulp')),
 	plugins = require('gulp-load-plugins')(),
 	args    = require('yargs').argv;
 
-plugins.gulpif = require('gulp-if');
-plugins.sprity = require('sprity');
+plugins.gulpif      = require('gulp-if');
+plugins.sprity      = require('sprity');
+plugins.runSequence = require('run-sequence');
 
 var cfg = require('../../gulpconfig.js')();
 
@@ -16,7 +17,11 @@ function swallowError(error) {
 	this.emit('end')
 }
 
-gulp.task('images', 'Optimize images and build Sprite.', ['images.optimize', 'images.sprite'], null, {
+gulp.task('images', 'Optimize images and build Sprite.', function(done){
+	plugins.runSequence('images.optimize', 'images.sprite', 'images.optimize.sprite', function () {
+		done();
+	});
+}, {
 	aliases: [cfg.tasks.alias + ':i'],
 	options: {
 		"dev":   "- run in development mode",
@@ -28,6 +33,7 @@ gulp.task('images.optimize', '- optimize images.', function () {
 
 	return gulp
 		.src(cfg.path.imagesDir + '**/*')
+		.pipe(plugins.gulpif(args.debug == true, plugins.debug()))
 		.pipe(plugins.imagemin(cfg.plugins.imagemin))
 		.pipe(gulp.dest(cfg.path.imagesDir));
 
@@ -35,12 +41,26 @@ gulp.task('images.optimize', '- optimize images.', function () {
 	aliases: [cfg.tasks.alias + ':io']
 });
 
-gulp.task('images.sprite', '- build sprite.', ['images.optimize'], function () {
+gulp.task('images.sprite', '- build sprite.', function () {
 
 	return plugins.sprity.src(cfg.plugins.sprity)
+				  .pipe(plugins.gulpif(args.debug == true, plugins.debug()))
 				  .on('error', swallowError)
 				  .pipe(plugins.gulpif('*.png', gulp.dest(cfg.path.imagesDir), gulp.dest(cfg.path.stylesDir)));
 
 }, {
 	aliases: [cfg.tasks.alias + ':is']
+});
+
+
+gulp.task('images.optimize.sprite', '- optimize images.', function () {
+
+	return gulp
+		.src(cfg.path.imagesDir + cfg.plugins.sprity.name + '.png')
+		.pipe(plugins.gulpif(args.debug == true, plugins.debug()))
+		.pipe(plugins.imagemin(cfg.plugins.imagemin))
+		.pipe(gulp.dest(cfg.path.imagesDir));
+
+}, {
+	aliases: [cfg.tasks.alias + ':ios']
 });
