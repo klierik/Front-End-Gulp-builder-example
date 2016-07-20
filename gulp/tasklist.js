7171e4e3aa9e43a4313dd151d00b96d9e200aaf8
klierik <leo-tv@yandex.ru>
2016-07-20 16:15:38 +0300

    added default task

---
+++ gulp/tasklist.js
@@ -0,0 +1,16 @@
+//
+// Gulp & plugins init
+var gulp    = require('gulp-help')(require('gulp')),
+	plugins = require('gulp-load-plugins')();
+
+plugins.runSequence = require('run-sequence');
+
+var cfg = require('../gulpconfig.js')();
+
+// Init Default gulp task
+gulp.task('default', 'Run all builds', function (done) {
+	plugins.runSequence('images', 'styles', function () {
+		// console.log('Run something else');
+		done();
+	});
+});
